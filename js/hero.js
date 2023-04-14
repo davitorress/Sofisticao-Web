62da063fc9fcfe39a5f1e79867c3c20d43095c1d
davitorress <dev.davitorres@gmail.com>
2023-04-14 18:09:29 -0300

    feat: clone progress bar for each hero item

--- js/hero.js
+++ js/hero.js
@@ -1,17 +1,26 @@
 let currentImage = 1;
+const carouselImages = document.querySelector(".carousel-images");
+
+for (let i = 1; i <= carouselImages.childElementCount; i++) {
+	let progressContainer = document.querySelector(".progress-bar-container").cloneNode(true);
+	console.log(progressContainer);
+	progressContainer.firstElementChild.id = "progress-bar-" + i;
+	progressContainer.style.display = "block";
+	document.querySelector(".carousel-progress").appendChild(progressContainer);
+}
+
 let progressBar = document.getElementById("progress-bar-" + currentImage);
 progressBar.style.width = "0%";
 
 const nextImage = () => {
 	progressBar.style.width = "0%";
 	currentImage++;
-	if (currentImage > 3) {
+	if (currentImage > carouselImages.childElementCount) {
 		currentImage = 1;
 	}
 	progressBar = document.getElementById("progress-bar-" + currentImage);
 	progressBar.style.width = "0%";
 
-	const carouselImages = document.querySelector(".carousel-images");
 	carouselImages.style.transform = "translateX(-" + (currentImage - 1) * 100 + "%)";
 
 	let progress = 0;
