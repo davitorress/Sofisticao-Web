1d179c4b3eed93cbb886189e37a1d098e5e767b0
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-23 14:21:47 -0300

    fix: trying to fix all

--- sw.js
+++ sw.js
@@ -79,107 +79,101 @@ const filesToCache = [
   "./assets/icon/truck.svg",
 ];
 
-self.addEventListener('install', (event) => {
-    console.log('[ServiceWorker] Install');
-    event.waitUntil(
-      caches.open(CACHE).then((cache) => {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(filesToCache);
-      })
-    );
-    // Add install popup
-    self.skipWaiting();
-    self.clients.matchAll().then((clients) => {
-      clients.forEach((client) => {
-        client.postMessage('New version installed. Please refresh.');
-      });
+self.addEventListener("install", (event) => {
+  console.log("[ServiceWorker] Install");
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+  self.clients.matchAll().then((clients) => {
+    clients.forEach((client) => {
+      client.postMessage("New version installed. Please refresh.");
     });
   });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('[ServiceWorker] Activate');
-    event.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[ServiceWorker] Activate");
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
           if (key !== CACHE) {
-            console.log('[ServiceWorker] Removing old cache', key);
+            console.log("[ServiceWorker] Removing old cache", key);
             return caches.delete(key);
           }
-        }));
-      })
-    );
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    console.log('[ServiceWorker] Fetch', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
 
-  let deferredPrompt;
+self.addEventListener("fetch", (event) => {
+  console.log("[ServiceWorker] Fetch", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-  self.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    event.preventDefault();
-    // Stash the event so it can be triggered later
-    deferredPrompt = event;
-    // Show a custom install button or trigger the install prompt
-    const installButton = document.getElementById('install-button');
-    installButton.style.display = 'block';
-    installButton.addEventListener('click', () => {
-      // Show the install prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-        // Clear the deferredPrompt variable
-        deferredPrompt = null;
-      });
+let deferredPrompt;
+
+self.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  const installButton = document.getElementById("install-button");
+  installButton.style.display = "block";
+  installButton.addEventListener("click", () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      deferredPrompt = null;
     });
   });
-  
-  // This is the "Offline page" service worker
-  
-  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-  
-  // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-  const offlineFallbackPage = "ToDo-replace-this-name.html";
-  
-  self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-      self.skipWaiting();
-    }
-  });
-  
-  self.addEventListener('install', async (event) => {
-    event.waitUntil(
-      caches.open(CACHE)
-        .then((cache) => cache.add(offlineFallbackPage))
-    );
-  });
-  
-  if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-  }
+});
 
-  self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-      event.respondWith((async () => {
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("install", async (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
         try {
           const preloadResp = await event.preloadResponse;
-  
+
           if (preloadResp) {
             return preloadResp;
           }
-  
+
           const networkResp = await fetch(event.request);
           const cache = await caches.open(CACHE);
           cache.put(event.request, networkResp.clone());
@@ -193,7 +187,7 @@ self.addEventListener('install', (event) => {
           const offlineResp = await cache.match(offlineFallbackPage);
           return offlineResp;
         }
-      })());
-    }
-  });
-  
+      })()
+    );
+  }
+});
