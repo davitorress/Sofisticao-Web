96b133d59894466ca00c406994aafc1953cd0e45
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-23 13:14:03 -0300

    feat: PWA sw configs and activation

---
+++ sw.js
@@ -0,0 +1,52 @@
+// create a PWA basic service worker
+// https://developers.google.com/web/fundamentals/primers/service-workers/
+
+// cache name
+var cacheName = 'pwa-basic-v1';
+
+// files to cache
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+// install event
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+// fetch event
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+
