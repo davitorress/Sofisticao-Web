bb0175aef6b09e57024d8960c3dc3922f9b75e4c
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-23 13:46:19 -0300

    feat: sw updates, thanks gpt haha

--- sw.js
+++ sw.js
@@ -1,54 +1,57 @@
-// create a PWA basic service worker
-// https://developers.google.com/web/fundamentals/primers/service-workers/
-
 // cache name
-var cacheName = 'pwa-basic-v1';
+const cacheName = 'pwa-basic-v1';
 
 // files to cache
-var filesToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/main.js'
+const filesToCache = [
+'/',
+'/index.html',
+'/css/style.css',
+'/js/main.js'
 ];
 
 // install event
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', (event) => {
+console.log('[ServiceWorker] Install');
+event.waitUntil(
+caches.open(cacheName).then((cache) => {
+console.log('[ServiceWorker] Caching app shell');
+return cache.addAll(filesToCache);
+})
+);
+// Add install popup
+self.skipWaiting();
+self.clients.matchAll().then((clients) => {
+clients.forEach((client) => {
+client.postMessage('New version installed. Please refresh.');
+});
+});
 });
 
 // activate event
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
+self.addEventListener('activate', (event) => {
+console.log('[ServiceWorker] Activate');
+event.waitUntil(
+caches.keys().then((keyList) => {
+return Promise.all(keyList.map((key) => {
+if (key !== cacheName) {
+console.log('[ServiceWorker] Removing old cache', key);
+return caches.delete(key);
+}
+}));
+})
+);
+return self.clients.claim();
 });
 
 // fetch event
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+console.log('[ServiceWorker] Fetch', event.request.url);
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
 });
-
 
 // This is the "Offline page" service worker
 
@@ -60,40 +63,40 @@ const CACHE = "pwabuilder-page";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+if (event.data && event.data.type === "SKIP_WAITING") {
+self.skipWaiting();
+}
 });
 
 self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
+event.waitUntil(
+caches.open(CACHE)
+.then((cache) => cache.add(offlineFallbackPage))
+);
 });
 
 if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
+workbox.navigationPreload.enable();
 }
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+if (event.request.mode === 'navigate') {
+event.respondWith((async () => {
+try {
+const preloadResp = await event.preloadResponse;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+if (preloadResp) {
+    return preloadResp;
   }
+
+  const networkResp = await fetch(event.request);
+  return networkResp;
+} catch (error) {
+
+  const cache = await caches.open(CACHE);
+  const cachedResp = await cache.match(offlineFallbackPage);
+  return cachedResp;
+}
+})());
+}
 });
