e07aabb3a5dbc7979cb596da740f6d04569434f3
davitorress <dev.davitorres@gmail.com>
2023-04-24 20:29:18 -0300

    fix: links on buttons

--- js/home.js
+++ js/home.js
@@ -2,3 +2,10 @@ import "./home/hero.js";
 import "./home/categories.js";
 // import "./home/ratings.js";
 import "./home/bestSellers.js";
+
+document.querySelector("button.highlight-action").addEventListener("click", () => {
+	window.location.href = "shop.html";
+});
+document.querySelector("button.discount-action").addEventListener("click", () => {
+	window.location.href = "shop.html";
+});
