11253941e4354469b691d9fa01e1e62d19c4a4c4
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-24 02:16:39 -0300

    feat: offline page

--- sw.js
+++ sw.js
@@ -1,73 +1,11 @@
 // cache name
-const CACHE = "pwa-sofisticao-v3";
+const CACHE = "pwa-sofisticao-v4";
 
 // files to cache
 const filesToCache = [
-  "index.html",
-  "css/about.css",
-  "css/bestSellers.css",
-  "css/categories.css",
-  "css/discount.css",
-  "css/dispatch.css",
-  "css/footer.css",
-  "css/globalStyles.css",
-  "css/hero.css",
-  "css/highlight.css",
-  "css/pets.css",
-  "css/rating.css",
-  "css/shop/shop.css",
-  "css/shop/menuFilter.css",
-  "css/shop/menuAnimation.css",
-  "css/shop/headerDesk.css",
-  "js/ratings.js",
-  "js/navbar-shop.js",
-  "js/menuFilters.js",
-  "js/menuBurguer.js",
-  "js/hero.js",
-  "js/categories.js",
-  "assets/img/about.png",
-  "assets/img/black-logo.svg",
-  "assets/img/desktop-logo.svg",
-  "assets/img/discount-lg.png",
-  "assets/img/discount.png",
-  "assets/img/hero-img.png",
-  "assets/img/highlight.png",
-  "assets/img/highlight.svg",
-  "assets/img/mobile-logo.png",
-  "assets/img/mobile-logo.svg",
-  "assets/img/mothers-day_mobile.png",
-  "assets/img/mothers-day.png",
+  "offline.html",
+  "css/pwa.css",
   "assets/img/orange-logo.svg",
-  "assets/img/sofisticao-white.png",
-  "assets/img/sofisticao-white.svg",
-  "assets/img/sofisticao.png",
-  "assets/img/white-logo.svg",
-  "assets/img/icons/back-page.svg",
-  "assets/img/icons/search.svg",
-  "assets/img/icons/shopping-bag.svg",
-  "assets/fonts/Estedad[wght,kshd].ttf",
-  "assets/icon/chevron-down.svg",
-  "assets/icon/chevron-left.svg",
-  "assets/icon/chevron-right.svg",
-  "assets/icon/clothes.svg",
-  "assets/icon/filter.svg",
-  "assets/icon/gift.svg",
-  "assets/icon/heart-white.svg",
-  "assets/icon/heart.svg",
-  "assets/icon/instagram.svg",
-  "assets/icon/mail.svg",
-  "assets/icon/map-pin.svg",
-  "assets/icon/menu.svg",
-  "assets/icon/next-page.svg",
-  "assets/icon/phone.svg",
-  "assets/icon/search-white.svg",
-  "assets/icon/search.svg",
-  "assets/icon/shop-store.svg",
-  "assets/icon/shopping-bag-white.svg",
-  "assets/icon/shopping-bag.svg",
-  "assets/icon/star-filled.svg",
-  "assets/icon/star.svg",
-  "assets/icon/truck.svg",
 ];
 
 self.addEventListener("install", (event) => {
@@ -122,11 +60,8 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
 
-const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -134,37 +69,42 @@ self.addEventListener("message", (event) => {
   }
 });
 
-self.addEventListener("install", async (event) => {
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-          if (preloadResp) {
-            return preloadResp;
-          }
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
-          const networkResp = await fetch(event.request);
-          const cache = await caches.open(CACHE);
-          cache.put(event.request, networkResp.clone());
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(event.request);
-          if (cachedResp) {
-            return cachedResp;
-          }
-          const offlineResp = await cache.match(offlineFallbackPage);
-          return offlineResp;
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-      })()
-    );
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });
