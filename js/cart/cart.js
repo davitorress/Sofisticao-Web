3e4c0fe4ddcd0f443f4754b4d830ac7c08f727b1
RianYuri <rianyurirgt@gmail.com>
2023-04-24 22:57:15 -0300

    feat: add

--- js/cart/cart.js
+++ js/cart/cart.js
@@ -7,13 +7,25 @@ const product = await fetch(
     .then((res) => res.json())  
     .then((data) => data);
 
+    // more
+    // qtd-btn
+    // less
+    const more_btn = document.getElementById("more");
+    const less_btn = document.getElementById("less");
+const qtdProducts = document.getElementById("qtd-btn");
+let number_qtd = 1;
+let temporario = product.preco;
+    more_btn.addEventListener("click", ()=>{
+        number_qtd++;
+        console.log(number_qtd);
+        console.log(temporario);
+    });
     const content_section = document.querySelector(".buy-product__container");
     const content_cart = content_section.querySelector(".buy-product__imgDesc");
     content_cart.querySelector("img").src = product.imagem;
 content_cart.querySelector(".title-product").innerText = product.nome;
 content_cart.querySelector(".tamanho").innerText = product.porte;
-content_cart.querySelector(".price-num__cart").innerText = "R$" + product.preco ;
+content_cart.querySelector(".price-num__cart").innerText = "R$" + temporario * number_qtd;
 content_cart.querySelector(".price-discount__cart").innerText = "R$" +product.preco_desc;
-
 
   
