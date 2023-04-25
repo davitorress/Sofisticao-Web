a656f65f61d2823e4ff142779d0f646bf9480021
RianYuri <rianyurirgt@gmail.com>
2023-04-25 20:34:38 -0300

    feat: add products cart

--- js/cart/cart.js
+++ js/cart/cart.js
@@ -1,31 +1,41 @@
 const product = await fetch(
-    "http://localhost:8000/product/" + window.location.search.split("=")[1],
-    {
-      mode: "cors",
-    }
-  )
-    .then((res) => res.json())  
-    .then((data) => data);
+  "http://localhost:8000/product/" + window.location.search.split("=")[1],
+  {
+    mode: "cors",
+  }
+)
+  .then((res) => res.json())
+  .then((data) => data);
 
-    // more
-    // qtd-btn
-    // less
-    const more_btn = document.getElementById("more");
-    const less_btn = document.getElementById("less");
+// more
+// qtd-btn
+// less
+const more_btn = document.getElementById("more");
+const less_btn = document.getElementById("less");
 const qtdProducts = document.getElementById("qtd-btn");
 let number_qtd = 1;
-let temporario = product.preco;
-    more_btn.addEventListener("click", ()=>{
-        number_qtd++;
-        console.log(number_qtd);
-        console.log(temporario);
-    });
-    const content_section = document.querySelector(".buy-product__container");
-    const content_cart = content_section.querySelector(".buy-product__imgDesc");
-    content_cart.querySelector("img").src = product.imagem;
+qtdProducts.innerHTML = number_qtd;
+
+let temporario = product.preco * number_qtd;
+
+more_btn.addEventListener("click", () => {
+  number_qtd++;
+  qtdProducts.innerHTML = number_qtd;
+});
+less_btn.addEventListener("click", () => {
+  if (number_qtd === 1) {
+    return;
+  } else {
+    number_qtd--;
+  }
+  qtdProducts.innerHTML = number_qtd;
+});
+
+const content_section = document.querySelector(".buy-product__container");
+const content_cart = content_section.querySelector(".buy-product__imgDesc");
+content_cart.querySelector("img").src = product.imagem;
 content_cart.querySelector(".title-product").innerText = product.nome;
 content_cart.querySelector(".tamanho").innerText = product.porte;
-content_cart.querySelector(".price-num__cart").innerText = "R$" + temporario * number_qtd;
-content_cart.querySelector(".price-discount__cart").innerText = "R$" +product.preco_desc;
-
-  
+content_cart.querySelector(".price-num__cart").innerHTML = "R$" + temporario;
+content_cart.querySelector(".price-discount__cart").innerText =
+  "R$" + product.preco_desc;
